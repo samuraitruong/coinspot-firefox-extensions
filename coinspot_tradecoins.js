833cc93b7efbaae27c23c6545bcd25d4a191b0f1
Truong Nguyen <tnguyen@demystdata.com>
2021-02-21 23:04:06 +1100

    rename and finalise chart

---
+++ coinspot_tradecoins.js
@@ -0,0 +1,16 @@
+function insertChart(d, header) {
+  document.querySelectorAll('table tr').forEach((el, index) => {
+    var td = document.createElement('td');
+    let label = header || 'Last ' + d + ' Days';
+
+    if (index > 0) {
+      label = `<img src="http://localhost:3000/chart?coin=${el.getAttribute(
+        'data-coin',
+      )}&width=100&height=50&period=${d}&lineWidth=0.7" />`;
+    }
+    td.innerHTML = label;
+    el.appendChild(td);
+  });
+}
+insertChart(3);
+insertChart(7);
