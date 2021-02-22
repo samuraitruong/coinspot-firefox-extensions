9e198509cb25f425991ec2fe667c1eb1ab541eca
Truong Nguyen <tnguyen@demystdata.com>
2021-02-22 20:38:06 +1100

    style update

--- content_scripts/coinspot_1.js
+++ content_scripts/coinspot_1.js
@@ -1,15 +1,20 @@
 function insertChart(d, header) {
-  document.querySelectorAll('table tr').forEach((el, index) => {
+  let index = 0;
+  document.querySelectorAll('table tr').forEach((el) => {
     var td = document.createElement('td');
     let label = header || 'Last ' + d + ' Days';
-
-    if (index > 0) {
-      label = `<img alt="chart" src="https://coinspot-chart.herokuapp.com/chart?coin=${el.getAttribute(
-        'data-coin',
-      )}&width=100&height=50&period=${d}&lineWidth=0.7" />`;
+    const coin = el.getAttribute('data-coin');
+    if (coin) {
+      const td = document.createElement('td');
+      td.innerHTML = `<img alt="chart" src="https://coinspot-chart.herokuapp.com/chart?coin=${coin}&width=100&height=50&period=${d}&lineWidth=0.7" />`;
+      el.appendChild(td);
+    } else {
+      const th = document.createElement('th');
+      th.innerHTML = label;
+      el.appendChild(th);
     }
-    td.innerHTML = label;
-    el.appendChild(td);
+
+    index++;
   });
 }
 
--- popup/coinspot.js
+++ popup/coinspot.js
@@ -1,17 +1,32 @@
 function runScriptOnContentPage(days) {
-  browser.tabs
-    .executeScript({
-      file: '/content_scripts/coinspot_1.js',
-    })
-    .then(() => {
-      browser.tabs.executeScript({ code: `insertChart(${days})` });
-    })
-    .catch((err) => alert(err));
+  if (chrome.tags) {
+    chrome.tabs.executeScript(
+      {
+        file: '/content_scripts/coinspot_1.js',
+      },
+      (err) => {
+        chrome.tabs.executeScript({ code: `insertChart(${days})` });
+      },
+    );
+  } else {
+    browser.tabs
+      .executeScript({
+        file: '/content_scripts/coinspot_1.js',
+      })
+      .then(() => {
+        browser.tabs.executeScript({ code: `insertChart(${days})` });
+      })
+      .catch((err) => alert(err));
+  }
 }
 
 document.querySelectorAll('button').forEach((el) => {
-  const period = +el.getAttribute('data-period');
+  let period = el.getAttribute('data-period');
   el.addEventListener('click', () => {
-    runScriptOnContentPage(period);
+    if (period === 'custom') {
+      period = document.querySelector('#custom').value;
+    }
+
+    runScriptOnContentPage(+period);
   });
 });
