e1149d8373f273f7fe98b525634ed2f83fbb256d
Truong Nguyen <tnguyen@demystdata.com>
2021-02-24 12:59:43 +1100

    WIP add setting to allow configure readonly api to fetch some data

--- setting/ui.js
+++ setting/ui.js
@@ -1,22 +1,33 @@
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.sync.set({
-    color: document.querySelector('#color').value,
+    data: JSON.stringify({
+      key: document.querySelector('#key').value,
+      secret: document.querySelector('#secret').value,
+    }),
   });
+  const span = document.createElement('span');
+  span.innerText = 'Setting saved.';
+  document.querySelector('body').appendChild(span);
+  setTimeout(() => {
+    span.remove();
+  }, 5000);
 }
 
 function restoreOptions() {
   function setCurrentChoice(result) {
-    document.querySelector('#color').value = result.color || 'blue';
+    const value = JSON.parse(result.data);
+    document.querySelector('#key').value = value.key;
+    document.querySelector('#secret').value = value.secret;
   }
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  let getting = browser.storage.sync.get('color');
+  let getting = browser.storage.sync.get('data');
   getting.then(setCurrentChoice, onError);
 }
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector('form').addEventListener('submit', saveOptions);
+document.addEventListener('DOMContentLoaded', restoreOptions);
